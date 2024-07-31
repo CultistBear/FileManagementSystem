50c4784d5c4552e242702a7621c2502938de8a0b
Kamya Singh <79724656+CultistBear@users.noreply.github.com>
2024-07-31 22:06:36 +0530

    Updating Files to Latest Version

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
-import os
 import pathlib
+import os
 #from cryptography.fernet import Fernet
 #Fernet.generate_key()
 FLASK_SECRET_KEY = os.environ['FLASK_SECRET']
@@ -8,4 +8,6 @@ DB_PASSWORD = os.environ['MYSQL_PASS']
 DATABASE_NAME = os.environ['FMS_DB']
 PASSWORD_SALT = os.environ['FMS_SALT']
 FILE_NAME_KEY = str.encode(os.environ["FILE_NAME_KEY"])
+FILE_SHARE_KEY = str.encode(os.environ["FILE_SHARE_KEY"])
 CURRENT_WORKING_DIRECTORY = pathlib.Path(__file__).parent.resolve()
+TIME_OF_DAY=['Night','Morning','Afternoon','Evening']
